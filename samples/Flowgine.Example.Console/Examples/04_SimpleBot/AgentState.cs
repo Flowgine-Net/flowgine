a34fae2957bef9414b7bf487d07d2ffeb1e1d456
Martin Golan <golan@musenaut.com>
2025-10-12 18:26:20 +0200

    feat: basic of basic chatbot

---
+++ samples/Flowgine.Example.Console/Examples/04_SimpleBot/AgentState.cs
@@ -0,0 +1,14 @@
+﻿using Flowgine.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flowgine.Example.Console.Examples._04_SimpleBot
+{
+    public class AgentState
+    {
+        public List<string> Messages { get; set; } = new();
+    }
+}
