f1e0eb601801b6f04909cc12be589491e2083c5f
Martin Golan <golan@musenaut.com>
2025-10-12 18:26:20 +0200

    feat: basic of basic chatbot

---
+++ samples/Flowgine.Example.Console/Examples/04_SimpleBot/Run.cs
@@ -0,0 +1,13 @@
+﻿using Flowgine.Example.Console.Shared;
+
+namespace Flowgine.Example.Console.Examples._04_SimpleBot;
+
+public class Run: IExample
+{
+    public string Id => "04-simple-bot";
+    public string Title => "SimpleBot - How to integrate LLMs in our flows";
+    public async Task RunAsync(CancellationToken ct = default)
+    {
+
+    }
+}
