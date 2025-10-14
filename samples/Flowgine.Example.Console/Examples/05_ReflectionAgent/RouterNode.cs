7689211837ff174eeb6c1b4bbe4c171932eac4e9
Martin Golan <golan@musenaut.com>
2025-10-14 19:16:38 +0200

    fix: reflection agent

---
+++ samples/Flowgine.Example.Console/Examples/05_ReflectionAgent/RouterNode.cs
@@ -0,0 +1,17 @@
+﻿using Flowgine.Abstractions;
+using Flowgine.Abstractions.Helpers;
+
+namespace Flowgine.Example.Console.Examples._05_ReflectionAgent;
+
+public class RouterNode : INode<AgentState>
+{
+    public object? Invoke(AgentState state, Runtime runtime, CancellationToken ct = default)
+    {
+        if (state.Messages.Count > 6)
+        {
+            return Command.GotoOnly(FlowgineEdge.END);
+        }
+
+        return Command.GotoOnly(nameof(ReflectionNode));
+    }
+}
